b1c385a0637c79d986d4df3013a2804e7abdc776
d <93896606+duanchongyang@users.noreply.github.com>
2022-08-03 14:33:11 -0400

    sprint2

---
+++ app01/views/mail.py
@@ -0,0 +1,14 @@
+from django.shortcuts import HttpResponse
+from django.core.mail import send_mail
+
+
+def sendmail(request):
+    """ send email """
+    send_mail(subject='This is for practice',
+              message='Hi, I am testing if the codes work！',
+              from_email='jingyumengdcy@126.com',  # The sender's email
+              # recipient_list=['Yu.Meng.Jing@ibm.com'],  # The receiver's email
+              recipient_list=['h3c273wechat@gamil.com'],  # The receiver's email
+              fail_silently=False)
+
+    return HttpResponse("Send successfully!")
